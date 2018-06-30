9f8dae46851230badb76a6ddb1686329edda900b
wildomonges <setwildo31@gmail.com>
2018-06-29 22:38:20 -0400

    added knn for iris classification

--- iris_classification/iris.py
+++ iris_classification/iris.py
@@ -1,23 +1,64 @@
 """
 @author Wildo Monges
-This code is in the book named "Building Machine Learning
-Systems with Python - Willi Richert and Luis Pedro Coelho"
+This code is in the book named "Introduction to Machine Learning with Python"
+O REILLY - Andreas C. Muller & Sarah Guido
 I write the code for learn and apply machine learning purpose
 """
 
-from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
 # We load the data with load_iris from sklearn
-data = load_iris()
-features = data['data']
-features_names = data['feature_names']
-target = data['target']
+iris_dataset = load_iris()
+# Print relevant information
+print("Keys of iris_dataset \n{}".format(iris_dataset.keys()))
+print(iris_dataset['DESCR'][:193] + "\n...")
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Features names: {}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(type(iris_dataset['data'])))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+# Print the first 5 columns
+print("First five columns of data: \n{}".format(iris_dataset['data'][:5]))
+# Type of target
+print("Type of target: {}".format(type(iris_dataset['target'])))
+print("Shape of target: {}".format(iris_dataset['target'].shape))
+print("Target: \n{}".format(iris_dataset['target']))
 
-for t, marker, c in zip(range(3), ">ox", "rgb"):
-    # we plot each class
-    plt.scatter(features[target == t, 0],
-                features[target == t, 1],
-                marker=marker,
-                c=c)
+# 0 = Setosa, 1 = Versicolor, 2 = Virginica
+# Training and Testing Data
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'],
+                                                    iris_dataset['target'],
+                                                    random_state=0)
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+# create dataframe from data in X_train
+# label the columns using the string in iris_dataset.feature_names
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+# create a scatter matrix from the dataframe, color by y_train
+g = pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+                      hist_kwds={'bins': 20}, s=60, alpha=.8)
+plt.show()
+
+# build model using KNN
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new.shape: {}".format(X_new.shape))
+
+prediction = knn.predict(X_new)
+print("Prediction: {}".format(prediction))
+print("Predicted target name: {}".format(iris_dataset['target_names'][prediction]))
+
+# Evaluating model
+y_pred = knn.predict(X_test)
+print("Test set predictions: \n {}".format(y_pred))
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
